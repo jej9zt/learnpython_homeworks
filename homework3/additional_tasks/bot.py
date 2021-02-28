b6e685668ea47c866660834e05251a40a46e8f8f
Danil <edu@bestmail.us>
2021-02-28 16:26:41 +0500

    Исправил замечания по игре в города у бота

--- homework3/additional_tasks/bot.py
+++ homework3/additional_tasks/bot.py
@@ -3,7 +3,6 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 import ephem
 import re
-from random import choice
 import operator
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
@@ -65,59 +64,59 @@ def get_last_letter(word):
     return word_end_letter
 
 
-def get_citi(citi_end_letter, cities_list):
-    citi_answer_list = []
-    for citi in cities_list:
-        if re.match(rf'^{citi_end_letter}.*?', citi):
-            citi_answer_list.append(citi)
-    citi_answer = choice(citi_answer_list)
-    if citi_answer:
-        return citi_answer
-    return None
+def get_city(city_end_letter, cities_dict):
+    try:
+        return cities_dict[city_end_letter].pop()
+    except KeyError:
+        return None
 
 
-def cities_game_core(citi, user_data):
-    cities_list = user_data['cities_list']
+def cities_game_core(city, user_data):
+    cities_dict = user_data['cities_dict']
     cities_used = user_data['cities_used']
-    if citi in cities_list:
-        if len(cities_used) == 0:
-            cities_list.remove(citi)
-            citi_end_letter = get_last_letter(citi)
-            citi_answer = get_citi(citi_end_letter, cities_list)
-            cities_list.remove(citi_answer)
-            cities_used += (citi, citi_answer)
-        elif (len(cities_used) > 0 and
-              get_last_letter(cities_used[-1]) == citi[0].upper()):
-            cities_list.remove(citi)
-            citi_end_letter = get_last_letter(citi)
-            citi_answer = get_citi(citi_end_letter, cities_list)
-            if citi_answer:
-                cities_list.remove(citi_answer)
-                cities_used += (citi, citi_answer)
-            else:
-                return 'Ты победил, у меня больше нет вариантов!'
-        elif len(cities_used) > 0:
+    try:
+        if get_last_letter(cities_used[-1]) != city[0].upper():
             return f'Напиши город на букву {get_last_letter(cities_used[-1])}'
-        return f'{citi_answer}, ваш ход'
-    elif (citi not in cities_list and len(cities_used) > 0 and
-          get_last_letter(cities_used[-1]) != citi[0].upper()):
-        return f'Напиши город на букву {get_last_letter(cities_used[-1])}'
-    elif citi in cities_used:
-        return f'{citi} уже использовался, напиши другой'
-    return 'Я незнаю такого города, напиши другой'
+    except IndexError:
+        pass
+    if city in cities_used:
+        return f'{city} уже использовался, напиши другой'
+    try:
+        cities_dict[city[0]].remove(city)
+    except KeyError:
+        return 'Я незнаю такого города, напиши другой'
+    city_end_letter = get_last_letter(city)
+    city_answer = get_city(city_end_letter, cities_dict)
+    if city_answer:
+        cities_used += (city, city_answer)
+    else:
+        return 'Ты победил, у меня больше нет вариантов!'
+    return f'{city_answer}, ваш ход'
+
+
+def create_cities_dict(cities_list):
+    cities_dict = {}
+    for city in cities_list:
+        if re.match(fr'{city[0]}.*?', city):
+            if cities_dict.get(city[0]):
+                cities_dict[city[0]].add(city)
+            else:
+                cities_dict[city[0]] = {city}
+    return cities_dict
 
 
 def cities_game(update, context):
     if context.args:
-        citi = context.args[0]
-        if not context.user_data.get('cities_list'):
+        city = context.args[0].capitalize()
+        if not context.user_data.get('cities_dict'):
             context.user_data['cities_used'] = []
             with open('goroda.txt', 'r') as f:
-                context.user_data['cities_list'] = f.read().splitlines()
-            update.message.reply_text(cities_game_core(citi,
+                context.user_data['cities_dict'] = create_cities_dict(
+                    f.read().splitlines())
+            update.message.reply_text(cities_game_core(city,
                                                        context.user_data))
         else:
-            update.message.reply_text(cities_game_core(citi,
+            update.message.reply_text(cities_game_core(city,
                                                        context.user_data))
 
 
